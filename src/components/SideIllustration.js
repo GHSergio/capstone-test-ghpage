1977da8e345b4c51223cddecfaea43962d9340d4
Ming Hsu <n209202003@gmail.com>
2024-04-06 21:16:32 +0800

    loginPage toggle

--- src/components/SideIllustration.js
+++ src/components/SideIllustration.js
@@ -1,63 +1,65 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 import Counter from "./Counter/Counter";
 
-const SideIllustration = ({ title, text }) => {
-  const [activeCounter, setActiveCounter] = useState(1);
-
-  const handleArrowLeftClick = useCallback(() => {
-    setActiveCounter((prevCounter) => ((prevCounter - 2 + 3) % 3) + 1);
-  }, []);
-
-  const handleArrowRightClick = useCallback(() => {
-    setActiveCounter((prevCounter) => (prevCounter % 3) + 1);
-  }, []);
+const SideIllustration = ({
+  title,
+  text,
+  activeCounter,
+  background,
+  handleArrowLeftClick,
+  handleArrowRightClick,
+}) => {
+  const images = [
+    "https://s3-alpha-sig.figma.com/img/42da/1823/a1eea6bcd7c4273c62160f93ba61216d?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YPJytL0YTqXUhMszeJ9ByEid48C3o~v~0uz6BbmRgON~YrN7OsGTB5J0DyrnwI5~~-bSa29YUjapKRze8nyz1Va0jNLJRT5VJ~78kIo5U8mmh7-GbgzOCOKkipXPziQw35XeiPo~~1fHSVZKkgqqSUEfNwc1imxhHJmx-Gcrc5sRBUk-7pAOMMr-7EGthLt~vxKQOofNh6ZVbWVKrd2ab6IcG8wgKEXJgXt6jcn1PlSAVD6OvNa-POEXz3aZ~QPCnpzBGljZV8kRBteTo7RuROibpYTZPZhhiVT63oIArq93jpPVcZmScyu~14GRWy5spGbHnVI8YaAYh8~-OahuwQ__",
+    "https://s3-alpha-sig.figma.com/img/7a12/1840/b37291fdf4d627ca1fea05b85bb5897f?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XCCDI07tSYtiuhny5cL~Bzx6lZsHeMRBLKOHIlmXIff7hzsKHfsn-ctTxFsrcpU-IVzbhmPHUgNGveNVG-pQC4dTlX2RcdZS~csawA6v9sRWH2fvhYbuQjn4bkZ0a8ieYi4J-UV6ClRZVQHnW38FbS3MvtaN1Ec0tWTzlvKdKqN4bcS5C0ELsd3nt7-CqVpQb8BjfhXBm1PzogdOOQNroh-nhNGresebvwdnLS6AXJNWSZ6xjaKIx0GOyom0M02RS53ztePt7Suy7zA4ADuRf4zuZmqiDMYr4WiGC0FMGnKg1-fPRVL43wkPcCu8dflVr77to2uUZ04PWSzIRJcsHQ__",
+    "https://s3-alpha-sig.figma.com/img/52e2/b677/a83b9345284d0f807954cb64eabc0537?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nTue4OdKrhfOqtnUsw83LyD3OUYc1v7eKacvkduEFQ0OLsFtbLPM0EpRKNdyIeN6MKEUkZFJ4J~7dxJC5d98ghf-VK2a8b61oxTCMOkmATpjPjvbsN4FF~vR-S3pRkq1r8CWjc7GSlU1kjmObEb~KL4w5RH68Mkvzl12urUo3jcl65N5C0jNb4X-SC8YjXFG0vxWp3LVPhpA4-MSuMOp5-HKLuxaAsQEuEKjLsWjdTkdsg62yuXX25kqZBzu4ZK1HjbjoXViOjthfLPZIoaedMRFqzvo1A-lvwvbNBDR5yUeTyqgh4cUnPA78VJDbWG6DZ00-1g1C6B5ugFRS02ESQ__",
+  ];
 
   return (
     <>
-      <img
-        src="https://s3-alpha-sig.figma.com/img/42da/1823/a1eea6bcd7c4273c62160f93ba61216d?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=YPJytL0YTqXUhMszeJ9ByEid48C3o~v~0uz6BbmRgON~YrN7OsGTB5J0DyrnwI5~~-bSa29YUjapKRze8nyz1Va0jNLJRT5VJ~78kIo5U8mmh7-GbgzOCOKkipXPziQw35XeiPo~~1fHSVZKkgqqSUEfNwc1imxhHJmx-Gcrc5sRBUk-7pAOMMr-7EGthLt~vxKQOofNh6ZVbWVKrd2ab6IcG8wgKEXJgXt6jcn1PlSAVD6OvNa-POEXz3aZ~QPCnpzBGljZV8kRBteTo7RuROibpYTZPZhhiVT63oIArq93jpPVcZmScyu~14GRWy5spGbHnVI8YaAYh8~-OahuwQ__"
-        alt=""
-        className="font"
-      />
-      <div className="controller">
-        <div className="arrow-left" onClick={handleArrowLeftClick}>
-          <svg
-            width="13"
-            height="20"
-            viewBox="0 0 13 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12.6833 17.65L5.05 10L12.6833 2.35L10.3333 0L0.333336 10L10.3333 20L12.6833 17.65Z"
-              fill="white"
-            />
-          </svg>
+      <div className="SideIllustration" style={{ background: background }}>
+        <img src={images[activeCounter - 1]} alt="" className="font" />
+
+        <div className="controller">
+          <div className="arrow-left" onClick={handleArrowLeftClick}>
+            <svg
+              width="13"
+              height="20"
+              viewBox="0 0 13 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12.6833 17.65L5.05 10L12.6833 2.35L10.3333 0L0.333336 10L10.3333 20L12.6833 17.65Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+          <div className="arrow-right" onClick={handleArrowRightClick}>
+            <svg
+              width="13"
+              height="20"
+              viewBox="0 0 13 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.31665 17.65L7.94998 10L0.31665 2.35L2.66665 0L12.6667 10L2.66665 20L0.31665 17.65Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+          <div className="counters">
+            <Counter active={activeCounter === 1} />
+            <Counter active={activeCounter === 2} />
+            <Counter active={activeCounter === 3} />
+          </div>
         </div>
-        <div className="arrow-right" onClick={handleArrowRightClick}>
-          <svg
-            width="13"
-            height="20"
-            viewBox="0 0 13 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M0.31665 17.65L7.94998 10L0.31665 2.35L2.66665 0L12.6667 10L2.66665 20L0.31665 17.65Z"
-              fill="white"
-            />
-          </svg>
+        <div className="shadow"></div>
+        <div className="content">
+          <span className="title">{title}</span>
+          <p className="text">{text}</p>
         </div>
-        <div className="counters">
-          <Counter active={activeCounter === 1} />
-          <Counter active={activeCounter === 2} />
-          <Counter active={activeCounter === 3} />
-        </div>
-      </div>
-      <div className="shadow"></div>
-      <div className="content">
-        <span className="title">{title}</span>
-        <p className="text">{text}</p>
       </div>
     </>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import LoginWrapper from "../components/LoginWrapper.js";
 import SideIllustration from "../components/SideIllustration.js";
 import "../styles/loginContainer.scss";
 
 const Login = () => {
+  const [activeCounter, setActiveCounter] = useState(1);
+  const [background, setBackground] = useState("#23262f");
+
+  const handleArrowLeftClick = useCallback(() => {
+    setActiveCounter((prevCounter) =>
+      prevCounter === 1 ? 3 : prevCounter - 1
+    );
+    setBackground(getBackground(activeCounter - 1));
+  }, [activeCounter]);
+
+  const handleArrowRightClick = useCallback(() => {
+    setActiveCounter((prevCounter) =>
+      prevCounter === 3 ? 1 : prevCounter + 1
+    );
+    setBackground(getBackground(activeCounter + 1));
+  }, [activeCounter]);
+
+  const getBackground = (counter) => {
+    const colors = {
+      1: "#23262f",
+      2: "#2d3831",
+      3: "#063540",
+    };
+    return colors[counter];
+  };
+
+  const getTitleAndText = (counter) => {
+    switch (counter) {
+      case 1:
+        return {
+          title: "鼓舞人心的故事",
+          text: "從非凡的人生故事和成功經歷中獲得靈感",
+        };
+      case 2:
+        return {
+          title: "輕鬆分類與管理",
+          text: "一目了然的分類，讓收藏的 Podcast 保持整潔",
+        };
+      case 3:
+        return {
+          title: "Spotify 快速同步",
+          text: "透過 Spotify 登入，即刻同步您的收藏，隨時隨地收聽",
+        };
+      default:
+        return {
+          title: "Default Title",
+          text: "Default Text",
+        };
+    }
+  };
+
+  const { title, text } = getTitleAndText(activeCounter);
+
   return (
     <>
       <div className="login-container">
@@ -12,8 +65,12 @@ const Login = () => {
         </div>
         <div className="right-container">
           <SideIllustration
-            title="鼓舞人心的故事"
-            text="從非凡的人生故事和成功經歷中獲得靈感"
+            title={title}
+            text={text}
+            activeCounter={activeCounter}
+            background={background}
+            handleArrowLeftClick={handleArrowLeftClick}
+            handleArrowRightClick={handleArrowRightClick}
           />
         </div>
       </div>
